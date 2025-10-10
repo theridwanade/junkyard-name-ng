1f5e26393b665ab63d321b35fb377717d1bdb149
theridwanade <theridwanade@gmail.com>
2025-10-10 11:46:06 +0100

    feat: add project detail page and enhance styling
    - Implemented a new project detail page to display project descriptions.
    - Added dynamic fetching of project descriptions from GitHub README files.
    - Updated CSS styles for improved layout and readability.
    - Enhanced existing styles in main.css and input.css for better visual consistency.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 import morgan from "morgan";
 import path from "path";
 import fs from "fs/promises";
+import fetch from "node-fetch";
+import {marked} from "marked";
 
 const PORT = Number(process.env.PORT) || 3000;
 const app = express();
@@ -27,6 +29,31 @@ app.get("/", async (req, res) => {
     }
 });
 
+app.get("/projects/:projectId", async (req, res) => {
+    const { projectId } = req.params;
+    try {
+        const filePath = path.join(__dirname, "..", "public", "projects.json");
+        const data = await fs.readFile(filePath, "utf-8");
+        const projects = JSON.parse(data);
+        const project = projects.find((p: { name: string }) => p.name === projectId);
+
+        const descriptionUrl = `https://raw.githubusercontent.com/theridwanade/${project.name}/main/README.md`
+        const response = await fetch(descriptionUrl);
+
+        if (!response.ok) throw new Error("Failed to fetch Markdown");
+        const md = await response.text();
+        const html = await marked(md);
+
+        if (project) {
+            res.render("project", { title: project.name, description: html });
+        } else {
+            res.status(404).send("Project not found");
+        }
+    } catch (error) {
+        console.error("Error loading project:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 app.get("/healthz", (req, res) => {
   res.json({ status: "OK" });
@@ -35,3 +62,4 @@ app.get("/healthz", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
